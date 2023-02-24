6f7bad729af3dddf338645bc2fbf469f80b4b6f1
akhil814 <120368606+akhil814@users.noreply.github.com>
2023-02-24 10:49:41 +0530

    added kafa producer for assigning qc task assinment after creating orders

--- order-management-service/src/main/java/com/groyyo/order/management/service/OrderManagementServiceApplication.java
+++ order-management-service/src/main/java/com/groyyo/order/management/service/OrderManagementServiceApplication.java
@@ -1,4 +1,4 @@
-package com.groyyo.quality.management.service;
+package com.groyyo.order.management.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ order-management-service/src/main/java/com/groyyo/order/management/service/config/KafkaConfig.java
@@ -0,0 +1,36 @@
+package com.groyyo.order.management.service.config;
+
+import com.groyyo.order.management.service.dto.request.TaskAssinmentDto;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+@Configuration
+public class KafkaConfig {
+
+
+
+    @Bean
+    public ProducerFactory<String, TaskAssinmentDto> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TaskAssinmentDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
+
