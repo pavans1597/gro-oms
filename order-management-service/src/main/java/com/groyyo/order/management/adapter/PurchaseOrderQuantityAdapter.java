39a564d933e17bd6525e7385848a30bbad5fa67c
Gagan Soni <gagansoni0033@gmail.com>
2023-03-17 13:42:50 +0530

    bulk order upload happy flow ready

--- order-management-service/src/main/java/com/groyyo/order/management/adapter/PurchaseOrderQuantityAdapter.java
+++ order-management-service/src/main/java/com/groyyo/order/management/adapter/PurchaseOrderQuantityAdapter.java
@@ -76,7 +76,6 @@ public class PurchaseOrderQuantityAdapter {
 		return PurchaseOrderQuantityResponseDto
 				.builder()
 				.name(purchaseOrderQuantity.getName())
-				.uuid(purchaseOrderQuantity.getUuid())
 				.purchaseOrderId(purchaseOrderQuantity.getPurchaseOrderId())
 				.quantity(purchaseOrderQuantity.getQuantity())
 				.sizeId(purchaseOrderQuantity.getSizeId())
