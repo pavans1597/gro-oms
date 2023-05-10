b66e4dc619b8aad4c8f6b8e0f48e53b3ddd81850
Abhigyan21 <abhigyanmandal2@gmail.com>
2023-05-10 22:13:47 +0530

    Fix token deserialization

--- order-management-service/src/main/java/com/groyyo/order/management/controller/UserController.java
+++ order-management-service/src/main/java/com/groyyo/order/management/controller/UserController.java
@@ -1,18 +1,16 @@
 package com.groyyo.order.management.controller;
 
-import com.groyyo.core.base.common.dto.GroyyoUser;
 import com.groyyo.core.base.common.dto.ResponseDto;
 import com.groyyo.core.base.http.utils.HeaderUtil;
 import com.groyyo.core.dto.userservice.LineType;
 import com.groyyo.core.dto.userservice.UserResponseDto;
 import com.groyyo.core.enums.QcUserType;
-import com.groyyo.core.multitenancy.multitenancy.domain.entity.Tenant;
 import com.groyyo.core.multitenancy.multitenancy.util.TenantContext;
 import com.groyyo.order.management.dto.request.CheckersCountResponseDto;
 import com.groyyo.order.management.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,31 +26,31 @@ public class UserController {
 
 
     @GetMapping("{qcUserType}/fetch")
-    public ResponseDto<List<UserResponseDto>> getUsers(Authentication authentication,
+    public ResponseDto<List<UserResponseDto>> getUsers(JwtAuthenticationToken authentication,
                                                        @PathVariable("qcUserType") QcUserType qcUserType ,
                                                        @RequestParam LineType lineType) {
         String factoryId = HeaderUtil.getFactoryIdHeaderValue();
 
         log.info("Request received to get getUsers by FactoryId : {}", factoryId);
 
-        GroyyoUser userDetails = (GroyyoUser) authentication.getPrincipal();
-        ResponseDto<List<UserResponseDto>> lineUsers = userService.getUsersByLineType(userDetails.getOrgId(), factoryId, lineType,qcUserType);
+        String orgId = (String) authentication.getTokenAttributes().get("orgId");
+        ResponseDto<List<UserResponseDto>> lineUsers = userService.getUsersByLineType(orgId, factoryId, lineType,qcUserType);
 
         return (Objects.isNull(lineUsers) || lineUsers.getData().isEmpty()) ? ResponseDto.failure(" Users not found ") : ResponseDto.success(" Users retrieved successfully  ", lineUsers.getData());
     }
 
 
     @GetMapping("{qcUserType}/count")
-    public ResponseDto<CheckersCountResponseDto> getUsersCount(Authentication authentication,
+    public ResponseDto<CheckersCountResponseDto> getUsersCount(JwtAuthenticationToken authentication,
                                                                @PathVariable("qcUserType") QcUserType qcUserType ,
                                                                @RequestParam LineType lineType) {
         String factoryId = TenantContext.getTenantId();
 
         log.info("Request received to get getUsers by FactoryId : {}", factoryId);
 
-        GroyyoUser userDetails = (GroyyoUser) authentication.getPrincipal();
+        String orgId = (String) authentication.getTokenAttributes().get("orgId");
 
-        CheckersCountResponseDto lineUsers = userService.getUsersCountByLineType(userDetails.getOrgId(), factoryId, lineType,qcUserType);
+        CheckersCountResponseDto lineUsers = userService.getUsersCountByLineType(orgId, factoryId, lineType,qcUserType);
 
         return (Objects.isNull(lineUsers)) ? ResponseDto.failure(" Users not found ") : ResponseDto.success(" Users count successfully  ", lineUsers);
     }
