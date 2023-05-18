0b0de8074d98187fb64aeba37abce3fe86720cf3
Nipun <nipunaggarwal16@gmail.com>
2023-05-18 15:19:46 +0530

    factory id check added for adding / updating style

--- order-management-service/src/main/java/com/groyyo/order/management/db/service/StyleDbService.java
+++ order-management-service/src/main/java/com/groyyo/order/management/db/service/StyleDbService.java
@@ -1,28 +1,30 @@
 package com.groyyo.order.management.db.service;
 
+import java.util.List;
+
 import com.groyyo.core.sqlPostgresJpa.service.AbstractJpaService;
 import com.groyyo.order.management.entity.Style;
 
-import java.util.List;
-
 public interface StyleDbService extends AbstractJpaService<Style, Long> {
 
+	List<Style> getAllStyles(String factoryId);
 
-    List<Style> getAllStyles(String factoryId);
+	List<Style> getAllStylesForStatus(boolean status, String factoryId);
 
+	Style getStyleById(String id);
 
-    List<Style> getAllStylesForStatus(boolean status,String factoryId);
+	Style saveStyle(Style style);
 
-    Style getStyleById(String id);
+	Style activateDeactivateStyle(Style style, boolean status);
 
+	boolean isEntityExistsByStyleNumber(String styleNumber);
 
-    Style saveStyle(Style style);
+	Style findOrCreate(Style style);
 
-
-    Style activateDeactivateStyle(Style style, boolean status);
-
-
-    boolean isEntityExistsByStyleNumber(String styleNumber);
-
-    Style findOrCreate(Style style);
+	/**
+	 * @param styleNumber
+	 * @param factoryId
+	 * @return
+	 */
+	boolean isEntityExistsByStyleNumberAndFactoryId(String styleNumber, String factoryId);
 }
--- order-management-service/src/main/java/com/groyyo/order/management/service/impl/StyleServiceImpl.java
+++ order-management-service/src/main/java/com/groyyo/order/management/service/impl/StyleServiceImpl.java
@@ -5,9 +5,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import com.groyyo.order.management.adapter.StyleAdapter;
-import com.groyyo.order.management.entity.Product;
-import com.groyyo.order.management.entity.Style;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +16,10 @@ import com.groyyo.core.base.exception.RecordExistsException;
 import com.groyyo.core.base.http.utils.HeaderUtil;
 import com.groyyo.core.dto.PurchaseOrder.StyleDto;
 import com.groyyo.core.dto.fileManagement.dto.response.FileResponseDto;
+import com.groyyo.order.management.adapter.StyleAdapter;
 import com.groyyo.order.management.db.service.StyleDbService;
+import com.groyyo.order.management.entity.Product;
+import com.groyyo.order.management.entity.Style;
 import com.groyyo.order.management.http.service.FileManagementHttpService;
 import com.groyyo.order.management.service.StyleService;
 
@@ -88,7 +88,9 @@ public class StyleServiceImpl implements StyleService {
 
 		log.info("Serving request to add a style with request object:{}", styleRequestDto);
 
-		runValidations(styleRequestDto);
+		String factoryId = HeaderUtil.getFactoryIdHeaderValue();
+
+		runValidations(styleRequestDto, factoryId);
 
 		Style style = StyleAdapter.buildStyleFromRequest(styleRequestDto);
 
@@ -104,28 +106,6 @@ public class StyleServiceImpl implements StyleService {
 
 		return styleDto;
 	}
-
-//	@Override
-//	public StyleDto addBulkStyle(List<StyleDto> styleRequestDto) {
-//
-//		log.info("Serving request to add a style with request object:{}", styleRequestDto);
-//
-////		runValidations();
-//
-//		Style style = StyleAdapter.buildStyleFromRequest(styleRequestDto);
-//
-//		style = styleDbService.saveStyle(style);
-//
-//		if (Objects.isNull(style)) {
-//			log.error("Unable to add style for object: {}", styleRequestDto);
-//			return null;
-//		}
-//
-//		StyleDto styleDto = StyleAdapter.buildResponseFromEntity(style);
-//		setImagesForStyle(style, styleDto);
-//
-//		return styleDto;
-//	}
 
 	private void setImagesForStyle(Style style, StyleDto styleDto) {
 
@@ -160,7 +140,7 @@ public class StyleServiceImpl implements StyleService {
 			return null;
 		}
 
-		runValidations(styleUpdateDto);
+		runValidations(styleUpdateDto, null);
 
 		style = StyleAdapter.cloneStyleWithRequest(styleUpdateDto, style);
 
@@ -198,19 +178,21 @@ public class StyleServiceImpl implements StyleService {
 		styleDbService.saveStyle(style);
 	}
 
-	private boolean isEntityExistsWithStyleNumber(String styleNumber) {
+	private boolean isEntityExistsWithStyleNumber(String styleNumber, String factoryId) {
 
-		return StringUtils.isNotBlank(styleNumber) && styleDbService.isEntityExistsByStyleNumber(styleNumber);
+		return StringUtils.isNotBlank(styleNumber) && StringUtils.isBlank(factoryId) ? styleDbService.isEntityExistsByStyleNumber(styleNumber)
+				: Objects.nonNull(styleDbService.isEntityExistsByStyleNumberAndFactoryId(styleNumber, factoryId));
 	}
 
-	private void runValidations(StyleDto styleRequestDto) {
-		validateStyleNumber(styleRequestDto);
+	private void runValidations(StyleDto styleRequestDto, String factoryId) {
+
+		validateStyleNumber(styleRequestDto, factoryId);
 	}
 
-	private void validateStyleNumber(StyleDto styleRequestDto) {
+	private void validateStyleNumber(StyleDto styleRequestDto, String factoryId) {
 
-		if (isEntityExistsWithStyleNumber(styleRequestDto.getStyleNumber())) {
-			String errorMsg = "Style cannot be created/updated as record already exists with style number: " + styleRequestDto.getStyleNumber();
+		if (isEntityExistsWithStyleNumber(styleRequestDto.getStyleNumber(), factoryId)) {
+			String errorMsg = "Style cannot be created/updated as record already exists with style number: " + styleRequestDto.getStyleNumber() + " for factory id: " + factoryId;
 			throw new RecordExistsException(errorMsg);
 		}
 	}
